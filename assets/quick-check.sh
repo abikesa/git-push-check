32f13478a86cb02ca241fd2deefc680adcdca440
abikesa <144293697+abikesa@users.noreply.github.com>
2025-06-04 08:32:30 -0400

    Update quick-check.sh

--- assets/quick-check.sh
+++ assets/quick-check.sh
@@ -1,3 +1,4 @@
+python3 <(curl -s https://raw.githubusercontent.com/abikesa/git-push-check/refs/heads/main/assets/prune_check.py)  
 git lfs track
 du -sh *
 git rev-list --objects --all | sort -k 2 > allfiles.txt
